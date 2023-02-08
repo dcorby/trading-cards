0221e8176f4e2030af896117ab8c26daa30c1d7c
Your Name <you@example.com>
2023-02-07 23:19:41 -0500

    Working on the create set fragment mostly

--- app/src/main/java/com/example/tradingcards/ui/main/CreateSetFragment.kt
+++ app/src/main/java/com/example/tradingcards/ui/main/CreateSetFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.example.tradingcards.R
 import com.example.tradingcards.databinding.FragmentCreateSetBinding
 import com.example.tradingcards.viewmodels.CreateSetViewModel
+import java.io.File
 
 class CreateSetFragment : Fragment() {
 
@@ -33,9 +36,29 @@ class CreateSetFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.directory = arguments?.getString("directory", "") ?: ""
+
         binding.name.setText(viewModel.name, TextView.BufferType.EDITABLE)
         binding.name.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
             viewModel.name = binding.name.text.toString()
+        }
+
+        binding.create.setOnClickListener {
+            createSet()
+        }
+    }
+
+    private fun createSet() {
+        val pathStr: String = requireContext().filesDir.absolutePath + "/" + viewModel.name
+        val file = File(pathStr)
+        if (!file.exists()) {
+            file.mkdirs()
+
+            val bundle = Bundle()
+            bundle.putString("directory", file.parent?.toString() ?: "")
+            val navController =
+                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main)
+            navController.navigate(R.id.action_CreateSetFragment_to_SetFragment, bundle)
         }
     }
 }
