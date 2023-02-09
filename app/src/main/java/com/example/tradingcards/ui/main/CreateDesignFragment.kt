88e2dd63bcaee8046c23e767b6502c54af791d1f
Your Name <you@example.com>
2023-02-09 13:43:55 -0500

    Working on placing the rectangle in create design fragment

--- app/src/main/java/com/example/tradingcards/ui/main/CreateDesignFragment.kt
+++ app/src/main/java/com/example/tradingcards/ui/main/CreateDesignFragment.kt
@@ -1,15 +1,20 @@
 package com.example.tradingcards.ui.main
 
-import android.R
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
+import androidx.core.view.setMargins
 import androidx.fragment.app.Fragment
 import com.example.tradingcards.databinding.FragmentCreateDesignBinding
 import com.skydoves.colorpickerview.ColorPickerView
 import com.skydoves.colorpickerview.listeners.ColorListener
+import com.example.tradingcards.R
 
 
 class CreateDesignFragment : Fragment() {
@@ -36,6 +41,18 @@ class CreateDesignFragment : Fragment() {
         // Set the title
         requireActivity().title = "Create Design"
 
+        // Create a rectangle on click
+        binding.rectangle.setOnClickListener {
+            //val rectangleView = layoutInflater.inflate(R.layout.rectangle, binding.designView, false) as RelativeLayout
+            val rectangleView = RelativeLayout(requireContext())
+            val params = RelativeLayout.LayoutParams(100, 100)
+            params.setMargins(100, 100, 0, 0)
+            rectangleView.setBackgroundColor(Color.parseColor("#ff0000"));
+            rectangleView.layoutParams = params
+            binding.designView.addView(rectangleView)
+        }
+
+        // Init the color picker
         binding.colorPicker.setColorListener(ColorListener { color, fromUser ->
             //val linearLayout: LinearLayout = findViewById(R.id.linearLayout)
             //linearLayout.setBackgroundColor(color)
