ee6f1f1a92d90a9459e98f25b0a8a8c62e80d6b9
Your Name <you@example.com>
2023-02-12 17:43:33 -0500

    Implementing database classes

---
+++ app/src/main/java/com/example/tradingcards/db/Helper.kt
@@ -0,0 +1,55 @@
+package com.example.tradingcards.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+const val CREATE_CARDS = """
+CREATE TABLE IF NOT EXISTS cards (
+  id INTEGER PRIMARY KEY NOT NULL,
+  width INTEGER NOT NULL,
+  height INTEGER NOT NULL
+);
+"""
+const val CREATE_CARD_VIEWS = """
+CREATE TABLE IF NOT EXISTS card_views (
+  id INTEGER PRIMARY KEY NOT NULL,
+  card INTEGER NOT NULL,
+  width INTEGER NOT NULL,
+  height INTEGER NOT NULL,
+  margin_left INTEGER NOT NULL,
+  margin_top INTEGER NOT NULL,
+  FOREIGN KEY(card) REFERENCES cards(id)
+);"""
+
+class Helper(context: Context) : SQLiteOpenHelper(context, "tradingCards.db", null, 1) {
+
+    private val tables = hashMapOf<String, String>(
+        "cards" to CREATE_CARDS,
+        "card_views" to CREATE_CARD_VIEWS
+    )
+
+    private fun createTables(db: SQLiteDatabase?) {
+        if (db == null) {
+            throw Exception("Could not set up database")
+        }
+        tables.forEach { (key, value) ->
+            db.execSQL(value)
+        }
+    }
+
+    override fun onOpen(db: SQLiteDatabase?) {
+        createTables(db)
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        createTables(db)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        tables.forEach { (key, value) ->
+            db.execSQL("DROP TABLE IF EXISTS ${key}")
+        }
+        createTables(db)
+    }
+}
---
+++ app/src/main/java/com/example/tradingcards/db/Manager.kt
@@ -0,0 +1,80 @@
+package com.example.tradingcards.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.SQLException
+import android.database.sqlite.SQLiteConstraintException
+import android.database.sqlite.SQLiteDatabase
+
+class Manager(context: Context) {
+
+    private var context: Context = context
+    lateinit private var helper: Helper
+    lateinit private var database: SQLiteDatabase
+
+    @Throws(SQLException::class)
+    fun open(): Manager {
+        helper = Helper(context)
+        // this will call oncreate() & onopen()
+        database = helper.writableDatabase
+        return this
+    }
+
+    fun close() {
+        helper.close()
+    }
+
+    @Throws(SQLiteConstraintException::class)
+    fun insert(table: String, contentValues: ContentValues): Long {
+        // https://stackoverflow.com/questions/3421577/sqliteconstraintexception-not-caught
+        return database.insertOrThrow(table, null, contentValues)
+    }
+
+    // https://stackoverflow.com/questions/6293063/identifying-datatype-of-a-column-in-an-sqlite-android-cursor
+    fun fetch(query: String, args: Array<String>): ArrayList<HashMap<Any, Any>> {
+        val cursor = database.rawQuery(query, args)
+        val columns = cursor.columnCount
+        val arrayList: ArrayList<HashMap<Any, Any>> = ArrayList()
+        while (cursor.moveToNext()) {
+            val row = HashMap<Any, Any>(columns)
+            for (i in 0 until columns) {
+                when (cursor.getType(i)) {
+                    Cursor.FIELD_TYPE_FLOAT -> row[cursor.getColumnName(i)] = cursor.getFloat(i)
+                    Cursor.FIELD_TYPE_INTEGER -> row[cursor.getColumnName(i)] = cursor.getInt(i)
+                    Cursor.FIELD_TYPE_STRING -> row[cursor.getColumnName(i)] = cursor.getString(i)
+                }
+            }
+            arrayList.add(row)
+        }
+        cursor.close()
+        return arrayList
+    }
+
+    // Extract a column as a list, by key
+    fun fetch(query: String, args: Array<String>, key: String): ArrayList<Any> {
+        val tmp = fetch(query, args)
+        val list = ArrayList<Any>()
+        for (i in 0 until tmp.size) {
+            list.add(tmp[i][key]!!)
+        }
+        return list
+    }
+
+    fun exec(query: String, args: Array<String>) {
+        database.execSQL(query, args)
+    }
+
+    // transactions
+    fun beginTransaction() {
+        database.beginTransaction()
+    }
+
+    fun commitTransaction() {
+        database.setTransactionSuccessful()
+    }
+
+    fun endTransaction() {
+        database.endTransaction()
+    }
+}
