572e2769cb025041fa843f227ca53a6fa182a782
Your Name <you@example.com>
2023-02-21 00:27:02 -0500

    Fix for some sizing issues

--- app/src/main/java/com/example/tradingcards/ui/main/SaveImageFragment.kt
+++ app/src/main/java/com/example/tradingcards/ui/main/SaveImageFragment.kt
@@ -1,5 +1,7 @@
 package com.example.tradingcards.ui.main
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,17 +9,19 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
-import android.widget.RelativeLayout
-import android.widget.RelativeLayout.LayoutParams
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.tradingcards.MainReceiver
 import com.example.tradingcards.R
 import com.example.tradingcards.Utils
 import com.example.tradingcards.databinding.FragmentSaveImageBinding
-import com.example.tradingcards.designviews.DIM
 import com.example.tradingcards.viewmodels.SaveImageViewModel
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.file.Files
+
 
 class SaveImageFragment : Fragment() {
 
@@ -52,11 +56,12 @@ class SaveImageFragment : Fragment() {
 
         // For testing
         viewModel.link = "https://www.si.com/.image/t_share/MTY4MjYxMDk5MDc5NTQyMDM3/rickey-henderson-getty3jpg.jpg"
-        viewModel.width = 766.toFloat()
+        viewModel.width = 776.toFloat()
         viewModel.height = 1200.toFloat()
         // Fire is an HDPI device, and width will be ~511 dp
         // https://stackoverflow.com/questions/2025282/what-is-the-difference-between-px-dip-dp-and-sp
 
+        // Get screen dims
         val screenWidth = mainReceiver.getScreenDims().getValue("width").toFloat()
         val screenHeight = mainReceiver.getScreenDims().getValue("height").toFloat()
 
@@ -66,28 +71,27 @@ class SaveImageFragment : Fragment() {
         // ^ Not reliable, because onDraw() might come later, and because of keyboard
         val frameWidth = (screenWidth - 32).toFloat()
         val frameHeight = (screenHeight - 32 - 100).toFloat()
-        Log.v("TEST", "frameWidth=${frameWidth}")
-        Log.v("TEST", "frameHeight=${frameHeight}")
 
-        // Get image's pixel dims
-        val origWidth = Utils.convertPxToDp(requireContext(), viewModel.width.toFloat())
-        val origHeight = Utils.convertPxToDp(requireContext(), viewModel.height.toFloat())
-        Log.v("TEST", "origWidth=${origWidth}")
-        Log.v("TEST", "origHeight=${origHeight}")
+        // Get original dims
+        val origWidth = viewModel.width.toFloat()
+        val origHeight = viewModel.height.toFloat()
 
-        /* Get the actual image dimensions to render
-         * Shrink if either width or height is larger than frame
-         */
+        // Get image (actual) dims
         var imageWidth = origWidth
         var imageHeight = origHeight
+        var shrink: Float
+        // the 2/3 factor is arbitrary, to leave some padding
         if (origWidth > frameWidth) {
-            imageHeight *= (frameWidth / origWidth).toFloat()
+            shrink = frameWidth / origWidth * (2/3)
+            imageHeight *= shrink
             imageWidth = frameWidth
         } else if (origHeight > frameHeight) {
-            imageWidth *= (frameHeight / origHeight).toFloat()
+            shrink = frameHeight / origHeight * (2/3)
+            imageWidth *= shrink
             imageHeight = frameHeight
         }
 
+        // Set image dims
         val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.henderi01)
         binding.image.setImageDrawable(drawable)
         //binding.image.setImageResource(R.drawable.henderi01)
@@ -97,38 +101,52 @@ class SaveImageFragment : Fragment() {
         imageParams.leftMargin = ((frameWidth - imageWidth) / 2.0).toInt()
         imageParams.topMargin = ((frameHeight - imageHeight) / 2.0).toInt()
 
-        /*  Now add green cropper to select the cutout
-         *  It will have the aspect ratio of the root view (screenDims)
-         *  1) If the orig is smaller than the screen (both width and height),
-         *     size the cropper entirely within the image
-         *  2) If the orig is larger than the screen (width or height)
-         *     make the cropper as large as possible, and shrink by
-         *     ratio of image:orig
-         */
-
-        // 1)
-        var cropperWidth = 0.toFloat()
-        var cropperHeight = 0.toFloat()
-        if (origWidth < screenWidth && origHeight < screenHeight) {
-            cropperWidth = origWidth
-            cropperHeight = origWidth * (screenHeight / screenWidth)
+        // Get cropper dims
+        var cropperWidth = screenWidth
+        var cropperHeight = screenHeight
+        while (cropperWidth > imageWidth || cropperHeight > imageHeight) {
+            cropperWidth -= 1
+            cropperHeight = cropperWidth * (screenHeight / screenWidth)
         }
-        // 2) TODO: Not implemented
 
-
-        // Display the cropper
+        // Set cropper dims
         val cropperParams = binding.cropper.layoutParams as FrameLayout.LayoutParams
         cropperParams.width = cropperWidth.toInt()
         cropperParams.height = cropperHeight.toInt()
         cropperParams.leftMargin = ((frameWidth - cropperWidth) / 2.0).toInt()
         cropperParams.topMargin = ((frameHeight - cropperHeight) / 2.0).toInt()
-        //Log.v("TEST", "cropper width=${cropperParams.width}")
-        //Log.v("TEST", "cropper height=${cropperParams.height}")
-        //Log.v("TEST", "cropper leftMargin=${cropperParams.leftMargin}")
-        //Log.v("TEST", "cropper topMargin=${cropperParams.topMargin}")
         binding.cropper.layoutParams = cropperParams
         binding.cropper.setOnTouchListener(onTouchListener)
 
+        // Save the image
+        binding.button.setOnClickListener {
+
+            // Crop the orig
+            val cropperOrigin = IntArray(2)
+            binding.cropper.getLocationOnScreen(cropperOrigin)
+            val imageOrigin = IntArray(2)
+            binding.image.getLocationOnScreen(imageOrigin)
+
+            val pctLeft = (cropperOrigin[0] - imageOrigin[0]) / imageWidth
+            val pctTop = (cropperOrigin[1] - imageOrigin[1]) / imageHeight
+
+            val left = pctLeft * origWidth
+            val top = pctTop * origHeight
+            val width = (binding.cropper.width / imageWidth) * origWidth
+            val height = (binding.cropper.height / imageHeight) * origHeight
+
+            val bitmap = drawable!!.toBitmap(origWidth.toInt(), origHeight.toInt())
+            val resized = Bitmap.createBitmap(bitmap, left.toInt(), top.toInt(), width.toInt(), height.toInt())
+
+            Log.v("TEST", "Writing to=${requireContext().filesDir.toString() + "/images/henderi01.jpg"}")
+            val file = File(requireContext().filesDir.toString() + "/images/henderi01.jpg")
+            if (!file.parentFile.exists()) {
+                file.parentFile.mkdir()
+            }
+            val fos = FileOutputStream(file)
+            resized.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+            fos.close()
+        }
     }
 }
 
