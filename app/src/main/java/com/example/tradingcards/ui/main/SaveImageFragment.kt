4712537c9f58d434b56c3234d14d04de98e23266
Your Name <you@example.com>
2023-02-20 20:26:35 -0500

    Working on the save image fragment

--- app/src/main/java/com/example/tradingcards/ui/main/SaveImageFragment.kt
+++ app/src/main/java/com/example/tradingcards/ui/main/SaveImageFragment.kt
@@ -3,9 +3,12 @@ package com.example.tradingcards.ui.main
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.RelativeLayout
+import android.widget.RelativeLayout.LayoutParams
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +16,7 @@ import com.example.tradingcards.MainReceiver
 import com.example.tradingcards.R
 import com.example.tradingcards.Utils
 import com.example.tradingcards.databinding.FragmentSaveImageBinding
+import com.example.tradingcards.designviews.DIM
 import com.example.tradingcards.viewmodels.SaveImageViewModel
 
 class SaveImageFragment : Fragment() {
@@ -53,12 +57,15 @@ class SaveImageFragment : Fragment() {
         // Fire is an HDPI device, and width will be ~511 dp
         // https://stackoverflow.com/questions/2025282/what-is-the-difference-between-px-dip-dp-and-sp
 
+        val screenWidth = mainReceiver.getScreenDims().getValue("width").toFloat()
+        val screenHeight = mainReceiver.getScreenDims().getValue("height").toFloat()
+
         // Get frame dims
         // Log.v("TEST", "width=${binding.frame.width}")
         // Log.v("TEST", "height=${binding.frame.height}")
         // ^ Not reliable, because onDraw() might come later, and because of keyboard
-        val frameWidth = (mainReceiver.getScreenDims().getValue("width") - 32).toFloat()
-        val frameHeight = (mainReceiver.getScreenDims().getValue("height") - 32 - 100).toFloat()
+        val frameWidth = (screenWidth - 32).toFloat()
+        val frameHeight = (screenHeight - 32 - 100).toFloat()
         Log.v("TEST", "frameWidth=${frameWidth}")
         Log.v("TEST", "frameHeight=${frameHeight}")
 
@@ -84,12 +91,11 @@ class SaveImageFragment : Fragment() {
         val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.henderi01)
         binding.image.setImageDrawable(drawable)
         //binding.image.setImageResource(R.drawable.henderi01)
-        val params = binding.image.layoutParams as FrameLayout.LayoutParams
-        params.width = imageWidth.toInt()
-        params.height = imageHeight.toInt()
-        params.leftMargin = ((frameWidth - imageWidth) / 2.0).toInt()
-        params.topMargin = ((frameHeight - imageHeight) / 2.0).toInt()
-
+        val imageParams = binding.image.layoutParams as FrameLayout.LayoutParams
+        imageParams.width = imageWidth.toInt()
+        imageParams.height = imageHeight.toInt()
+        imageParams.leftMargin = ((frameWidth - imageWidth) / 2.0).toInt()
+        imageParams.topMargin = ((frameHeight - imageHeight) / 2.0).toInt()
 
         /*  Now add green cropper to select the cutout
          *  It will have the aspect ratio of the root view (screenDims)
@@ -99,5 +105,68 @@ class SaveImageFragment : Fragment() {
          *     make the cropper as large as possible, and shrink by
          *     ratio of image:orig
          */
+
+        // 1)
+        var cropperWidth = 0.toFloat()
+        var cropperHeight = 0.toFloat()
+        if (origWidth < screenWidth && origHeight < screenHeight) {
+            cropperWidth = origWidth
+            cropperHeight = origWidth * (screenHeight / screenWidth)
+        }
+        // 2) TODO: Not implemented
+
+
+        // Display the cropper
+        val cropperParams = binding.cropper.layoutParams as FrameLayout.LayoutParams
+        cropperParams.width = cropperWidth.toInt()
+        cropperParams.height = cropperHeight.toInt()
+        cropperParams.leftMargin = ((frameWidth - cropperWidth) / 2.0).toInt()
+        cropperParams.topMargin = ((frameHeight - cropperHeight) / 2.0).toInt()
+        //Log.v("TEST", "cropper width=${cropperParams.width}")
+        //Log.v("TEST", "cropper height=${cropperParams.height}")
+        //Log.v("TEST", "cropper leftMargin=${cropperParams.leftMargin}")
+        //Log.v("TEST", "cropper topMargin=${cropperParams.topMargin}")
+        binding.cropper.layoutParams = cropperParams
+        binding.cropper.setOnTouchListener(onTouchListener)
+
+    }
+}
+
+val onTouchListener = object : View.OnTouchListener {
+    // https://stackoverflow.com/questions/7892853/how-to-use-correct-dragging-of-a-view-on-android/18806475#18806475
+    var prevX = 0
+    var prevY = 0
+    var params: FrameLayout.LayoutParams? = null
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if (v == null || event == null) { return false }
+        params = v.layoutParams as FrameLayout.LayoutParams
+
+        when (event.action) {
+            MotionEvent.ACTION_MOVE -> {
+                // Get the diff
+                val diffX = event.rawX.toInt() - prevX
+                val diffY = event.rawY.toInt() - prevY
+                params!!.leftMargin += diffX
+                params!!.topMargin += diffY
+                prevX = event.rawX.toInt()
+                prevY = event.rawY.toInt()
+                v.layoutParams = params
+                return true
+            }
+            MotionEvent.ACTION_UP -> {
+                return true
+            }
+            MotionEvent.ACTION_DOWN -> {
+                prevX = event.rawX.toInt()
+                prevY = event.rawY.toInt()
+
+                // little confused about bottom/right margins and the values??
+                params!!.bottomMargin = -2 * v.height
+                params!!.rightMargin = -2 * v.width
+                v.layoutParams = params
+                return true
+            }
+        }
+        return false
     }
 }
