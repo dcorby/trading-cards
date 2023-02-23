b9ebd62fdb1d210bfd52359d7b0d6998e08f7299
Your Name <you@example.com>
2023-02-22 21:56:22 -0500

    Working on select player fragment mostly

--- app/src/main/java/com/example/tradingcards/ui/main/SelectPlayerFragment.kt
+++ app/src/main/java/com/example/tradingcards/ui/main/SelectPlayerFragment.kt
@@ -1,7 +1,6 @@
 package com.example.tradingcards.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +9,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.Navigation
-import androidx.recyclerview.selection.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tradingcards.MainReceiver
 import com.example.tradingcards.R
@@ -26,7 +24,6 @@ class SelectPlayerFragment : Fragment() {
 
     private var _binding: FragmentSelectPlayerBinding? = null
     private val binding get() = _binding!!
-
     private lateinit var viewModel: SelectPlayerViewModel
     private lateinit var mainReceiver: MainReceiver
     private lateinit var dbManager: DBManager
@@ -55,39 +52,40 @@ class SelectPlayerFragment : Fragment() {
         recyclerView.adapter = playerAdapter
 
         // Get the current directory
-        viewModel.currentDirectory = arguments?.getString("currentDirectory", "") ?: ""
+        viewModel.currentDirectory = arguments?.getString("currentDirectory") ?: ""
         requireActivity().title = "Select Player (${viewModel.currentDirectory})"
 
         // Get the set data, to find out the source
         val set = dbManager.fetch("SELECT * FROM sets WHERE path = ?", arrayOf(viewModel.currentDirectory))[0]
         val source = set.getValue("source").toString()
+        binding.sourceLabel.text = source
 
-        binding.editText.addTextChangedListener {
+        binding.search.addTextChangedListener { text ->
             if (viewModel.job.isActive) {
                 viewModel.job.cancel()
             }
             var players: MutableList<PlayerItem> = mutableListOf()
             viewModel.job = viewModel.viewModelScope.launch(Dispatchers.IO) {
                 withTimeout(5000) {
-                    val params = arrayOf(source, it.toString() + "%")
-                    players = dbManager.fetch("SELECT * FROM players WHERE source = ? AND name LIKE ?", params).map {
-                        val id = it.getValue("id").toString()
-                        val name = it.getValue("name").toString()
-                        val imagePath = requireContext().filesDir.toString() + viewModel.currentDirectory + id + ".jpg"
-                        val hasImage = File(imagePath).exists()
-                        PlayerItem(id, name, hasImage)
-                    }.toMutableList()
-                    // players.add(0, PlayerItem("henderi01", "Rickey Henderson"))
-                    // delay(2000)
+                    if (text.toString() != "") {
+                        val params = arrayOf(source, text.toString() + "%")
+                        players =
+                            dbManager.fetch("SELECT * FROM players WHERE source = ? AND name LIKE ?", params).map {
+                                val id = it.getValue("id").toString()
+                                val name = it.getValue("name").toString()
+                                val imagePath =
+                                    requireContext().filesDir.toString() + viewModel.currentDirectory + id + ".jpg"
+                                val hasImage = File(imagePath).exists()
+                                PlayerItem(id, name, hasImage)
+                            }.toMutableList()
+                    }
                     // Switch back to the main application thread
                     withContext(Dispatchers.Main) {
                         playerAdapter.submitList(players)
                     }
                 }
             }
-            viewModel.job.invokeOnCompletion {
-                // anything we need to do here??
-            }
+            viewModel.job.invokeOnCompletion { }
         }
     }
 
