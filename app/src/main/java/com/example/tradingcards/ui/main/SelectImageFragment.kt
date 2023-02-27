9e386afa90a1fa80069281804bc6afa7920f8af5
Your Name <you@example.com>
2023-02-26 21:53:12 -0500

    Working on the image selector

--- app/src/main/java/com/example/tradingcards/ui/main/SelectImageFragment.kt
+++ app/src/main/java/com/example/tradingcards/ui/main/SelectImageFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tradingcards.BuildConfig
@@ -15,6 +16,9 @@ import com.example.tradingcards.adapters.ImageAdapter
 import com.example.tradingcards.databinding.FragmentSelectImageBinding
 import com.example.tradingcards.items.ImageItem
 import com.example.tradingcards.viewmodels.SelectImageViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONArray
 import org.json.JSONObject
 import java.net.URL
@@ -53,33 +57,39 @@ class SelectImageFragment : Fragment() {
         val recyclerView: RecyclerView = binding.recyclerView
         recyclerView.adapter = imageAdapter
 
-        val images = downloadImages()
-        imageAdapter.submitList(images)
+        downloadImages()
     }
 
-    private fun downloadImages() : MutableList<ImageItem> {
-        // Live
-        val url = "https://customsearch.googleapis.com/customsearch/v1?imgType=photo&q=${viewModel.name.toLowerCase().replace(" ", "+")}+baseball&searchType=image&cx=${BuildConfig.SEARCH_ENGINE_ID}&key=${BuildConfig.SEARCH_API_KEY}"
-        val response = URL(url)
-        val jsonObject = JSONObject(response.readText())
+    private fun downloadImages() {
+        viewModel.job = viewModel.viewModelScope.launch(Dispatchers.IO) {
+            // Live
+            val baseUrl = "https://customsearch.googleapis.com/customsearch/v1"
+            val name = viewModel.name.toLowerCase().replace(" ", "+")
+            val credentials = "cx=${BuildConfig.SEARCH_ENGINE_ID}&key=${BuildConfig.SEARCH_API_KEY}"
+            val url = "${baseUrl}?imgType=photo&q=${name}+baseball&searchType=image&${credentials}"
+            val response = URL(url)
+            val jsonObject = JSONObject(response.readText())
 
-        // Testing
-        //val jsonObject = JSONObject(Utils.readAssetsFile(requireContext(), "sample-api-response.json"))
+            // Testing
+            //val jsonObject = JSONObject(Utils.readAssetsFile(requireContext(), "sample-api-response.json"))
 
-        val images = mutableListOf<ImageItem>()
-        val items = Images.toMap(jsonObject).getValue("items") as ArrayList<HashMap<String, *>>
-        items.forEach { item ->
-            val link = item.getValue("link").toString()
-            val img = item.getValue("image") as HashMap<String, *>
-            val width = img.getValue("width") as Int
-            val height = img.getValue("height") as Int
-            val thumbnailLink = img.getValue("thumbnailLink").toString()
-            val thumbnailWidth = img.getValue("thumbnailWidth") as Int
-            val thumbnailHeight = img.getValue("thumbnailHeight") as Int
-            val image = ImageItem(link, width, height, thumbnailLink, thumbnailWidth, thumbnailHeight)
-            images.add(image)
+            val images = mutableListOf<ImageItem>()
+            val items = Images.toMap(jsonObject).getValue("items") as ArrayList<HashMap<String, *>>
+            items.forEach { item ->
+                val link = item.getValue("link").toString()
+                val img = item.getValue("image") as HashMap<String, *>
+                val width = img.getValue("width") as Int
+                val height = img.getValue("height") as Int
+                val thumbnailLink = img.getValue("thumbnailLink").toString()
+                val thumbnailWidth = img.getValue("thumbnailWidth") as Int
+                val thumbnailHeight = img.getValue("thumbnailHeight") as Int
+                val image = ImageItem(link, width, height, thumbnailLink, thumbnailWidth, thumbnailHeight)
+                images.add(image)
+            }
+            withContext(Dispatchers.Main) {
+                imageAdapter.submitList(images)
+            }
         }
-        return images
     }
 
     private fun adapterOnClick(imageItem: ImageItem) {
--- app/src/main/java/com/example/tradingcards/viewmodels/SelectImageViewModel.kt
+++ app/src/main/java/com/example/tradingcards/viewmodels/SelectImageViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.tradingcards.viewmodels
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Job
 
 class SelectImageViewModel : ViewModel() {
     var id = ""
     var name = ""
     var currentDirectory = ""
+    var job: Job = Job()
 }
